184b552d5179eebbf9c5d6c6ce85bf5fc215dcec
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-29 16:15:26 +0700

    Initial commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<div class="container">
+    <nav class="navbar d-flex py-4 align-items-center justify-content-between">
+        <a href="/" class="logo">Bugspy</a>
+    </nav>
+</div>
+
+<style>
+    .navbar {
+        border-bottom-width: 1px;
+        border-bottom-style: solid;
+        border-color: var(--gray-color);
+    }
+
+    .logo {
+        font-size: 1.5em;
+        color: var(--primary-color);
+        font-weight: var(--font-bold);
+    }
+</style>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,77 @@
+<div class="container">
+    <div class="py-5">
+        <div class="row align-items-center">
+            <div class="col-md-5">
+                <h1 class="title-md">Projects</h1>
+            </div>
+            <div class="col-md-7 d-flex gap-1">
+                <input
+                    type="text"
+                    class="input d-inline"
+                    placeholder="Enter keyword..."
+                />
+                <button class="btn btn-primary">Search</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-3">
+                <div class="card">
+                    <h3 class="card-title">Bugs Tracker</h3>
+                    <small class="issue">21 Issues</small>
+                    <div>
+                        <span class="progress-bar-value">50%</span>
+                        <div class="progress-bar">
+                            <div class="progress-bar-fill" />
+                        </div>
+                    </div>
+                    <div class="tags">
+                        <span class="tag">Javascript</span>
+                        <span class="tag">Web</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-3">
+                <div class="card">
+                    <h3 class="card-title">Web Links Management</h3>
+                    <small class="issue">21 Issues</small>
+                    <div>
+                        <span class="progress-bar-value">50%</span>
+                        <div class="progress-bar">
+                            <div class="progress-bar-fill" />
+                        </div>
+                    </div>
+                    <div class="tags">
+                        <span class="tag">Javascript</span>
+                        <span class="tag">Web</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-3">
+                <div class="card">
+                    <h3 class="card-title">TODOin Web Application</h3>
+                    <small class="issue">21 Issues</small>
+                    <div>
+                        <span class="progress-bar-value">50%</span>
+                        <div class="progress-bar">
+                            <div class="progress-bar-fill" />
+                        </div>
+                    </div>
+                    <div class="tags">
+                        <span class="tag">Javascript</span>
+                        <span class="tag">Web</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .issue {
+        display: block;
+        color: var(--gray-color-2);
+        font-weight: var(--font-medium);
+        margin-bottom: 1rem;
+    }
+</style>

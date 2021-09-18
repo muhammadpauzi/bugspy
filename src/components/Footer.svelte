0262a06b577153fffc19921a9219ccccf9d11186
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-09-18 11:32:23 +0700

    change to github logo in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,13 @@
+<script>
+    import Icon from "../shared/Icon.svelte";
+</script>
+
 <footer class="footer">
     <div class="container py-4">
         <div class="d-flex justify-content-between">
             <p>Created by Muhammad Pauzi.</p>
             <a href="https://github.com/muhammadpauzi/bugspy" target="_blank"
-                >Github</a
+                ><Icon name="github" /></a
             >
         </div>
     </div>
